ddf95dd00935e2cab1b787fb1872aaf476165691
JValtteri <46611993+JValtteri@users.noreply.github.com>
2025-11-11 15:21:14 +0200

    Implemented timezone detection to clibrate unit test output

--- client/src/utils/test.utils.ts
+++ client/src/utils/test.utils.ts
@@ -5,15 +5,23 @@ describe("dateAndTimeToPosix", () => {
     test("make date", () => {
         let date = "2025-11-10";
         let time = "21:19";
-        let out = 1762802340;
+        let out = 1762809540 + timezoneOffset();
+                //1762795140
         expect(dateAndTimeToPosix(date, time)).toEqual(out);
     });
 });
 
 describe("posixToDateAndTime", () => {
     test("parse to date", () => {
-        let posix = 1762802340;
+        let posix = 1762809540 + timezoneOffset();
         let expected = "21:19 10.11.2025";
         expect(posixToDateAndTime(posix)).toEqual(expected);
     });
 });
+
+function timezoneOffset(): number {
+    const now = new Date();
+    const timezoneOffsetMinutes = now.getTimezoneOffset();
+    const timezoneOffsetSeconds = timezoneOffsetMinutes*60;
+    return timezoneOffsetSeconds;
+}

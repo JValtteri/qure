e6d92e3c7cd3921c1d76ceb1139ffb0c67829077
JValtteri <46611993+JValtteri@users.noreply.github.com>
2025-11-25 15:57:29 +0200

    Added ReservationForm element

--- client/src/components/TimeslotList/TimeslotList.tsx
+++ client/src/components/TimeslotList/TimeslotList.tsx
@@ -1,22 +1,23 @@
 import './TimeslotList.css';
 
 import { type ReactNode } from 'react';
+import { type Signal } from "@preact/signals-react";
 import { useSignals } from "@preact/signals-react/runtime";
 
 import ListCard from '../common/ListCard/ListCard';
 
 import type { Timeslot } from "../../api/api";
-import { posixToTime } from '../../utils/utils';
+import { posixToDateAndTime, posixToTime } from '../../utils/utils';
 
 interface Props {
     timeslots: Map<Number, Timeslot>;
+    selectedSlot: Signal<number>;
 }
 
-function TimeslotList({timeslots}: Props) {
+function TimeslotList({timeslots, selectedSlot}: Props) {
     useSignals();
     console.log("Timeslots rendered");
-    //show: Signal<{ "selectedEventId": number, "eventID": number, "editor": boolean}>;
-    const children = makeChildren(timeslots);
+    const children = makeChildren(timeslots, selectedSlot);
     return (
         <>
             {timeslots.size === 0 && <p>No timeslots found</p>}
@@ -28,19 +29,18 @@ function TimeslotList({timeslots}: Props) {
 export default TimeslotList;
 
 
-const makeCard = (time: number, slots: number, reserved: number) => (
+const makeCard = (time: number, slots: number, reserved: number, selectedSlot: Signal<number>) => (
     <ListCard
         title={posixToTime(time)}
         startTime={""}
-        desc={""}
+        desc={posixToDateAndTime(time)}
         slots={slots}
         occupied={reserved}
         key={time}
         onClick={ () => {
-            //show.value = showIndex(index, event.ID)
-            //update(); ////////////////////////////////////////
+            selectedSlot.value = time
         } }
-        selected={false}//{ show.value.selectedEventId == index }
+        selected={ selectedSlot.value == time }
         className="timeslot-list-card"
     />
 )
@@ -48,19 +48,20 @@ const makeCard = (time: number, slots: number, reserved: number) => (
 function makeListElement(
     timeslot: Timeslot,
     index: number,
+    selectedSlot: Signal<number>
 ) {
     try {
-        return makeCard(index, timeslot.Size, timeslot.Reserved);
+        return makeCard(index, timeslot.Size, timeslot.Reserved, selectedSlot);
     } catch {
-        return makeCard(index, -1, -1);
+        return makeCard(index, -1, -1, selectedSlot);
     }
 };
 
-function makeChildren(timeslots: Map<Number, Timeslot>): ReactNode[] {
+function makeChildren(timeslots: Map<Number, Timeslot>, selectedSlot: Signal<number>): ReactNode[] {
     console.log("Rendered Children")
     let children: ReactNode[] = [];
     for (const [time, timeslot] of timeslots) {
-        children.push(makeListElement(timeslot, time.valueOf()));
+        children.push(makeListElement(timeslot, time.valueOf(), selectedSlot));
     }
     return children;
 }

67e8c982165a861990421b747e0922875e7297b8
JValtteri <46611993+JValtteri@users.noreply.github.com>
2025-12-09 20:52:41 +0200

    Refactored server function
    - improved clarity by extracting large low level portions to functions
    - Added handling for interrupts `Ctrl+C`, SIGTERM and SIGQUIT
    - Added support for go routines (WaitGroup)

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -3,33 +3,68 @@ package server
 import (
 	"fmt"
 	"log"
+	"sync"
+	"syscall"
+	"context"
 	"net/http"
+	"os/signal"
 )
 
 
+var wg sync.WaitGroup
+
 func Server() {
-	log.Println("Server UP")
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	defer stop()
 	LoadConfig(CONFIG_FILE)
+	setupHandlers()
+	go start()
+	<-ctx.Done()	// Wait for Ctrl+C or other stop signal
+	wg.Wait()		// Ensures registered processes are complete before exiting
+}
+
+func setupHandlers() {
 	http.HandleFunc("/", defaultRequest)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./static/css"))))
-	http.Handle("/js/",  http.StripPrefix("/js/",  http.FileServer(http.Dir("./static/js"))))
-	http.Handle("/img/", http.StripPrefix("/img/",  http.FileServer(http.Dir("./static/img"))))
-	http.HandleFunc("GET /api/events", 		getEvents)
-	http.HandleFunc("POST /api/event", 			getEvent)
-	http.HandleFunc("POST /api/session/auth", 	authenticateSession)
-	http.HandleFunc("POST /api/user/login", 	loginUser)
-	http.HandleFunc("POST /api/user/logout", 	logoutUser)
-	http.HandleFunc("POST /api/user/list", 		userReservations)
-	http.HandleFunc("POST /api/user/reserve", 	makeReservation)
-	http.HandleFunc("POST /api/user/register",	registerUser)
-	http.HandleFunc("POST /api/res/login", 		loginWithReservation)
-	http.HandleFunc("POST /api/admin/create", 	createEvent)
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./static/js"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./static/img"))))
+	http.HandleFunc("GET /api/events", getEvents)
+	http.HandleFunc("POST /api/event", getEvent)
+	http.HandleFunc("POST /api/session/auth", authenticateSession)
+	http.HandleFunc("POST /api/user/login", loginUser)
+	http.HandleFunc("POST /api/user/logout", logoutUser)
+	http.HandleFunc("POST /api/user/list", userReservations)
+	http.HandleFunc("POST /api/user/reserve", makeReservation)
+	http.HandleFunc("POST /api/user/register", registerUser)
+	http.HandleFunc("POST /api/res/login", loginWithReservation)
+	http.HandleFunc("POST /api/admin/create", createEvent)
+}
+
+func start() {
+	log.Println("Server UP")
 	if CONFIG.ENABLE_TLS {
-		log.Fatal(http.ListenAndServeTLS(
-			fmt.Sprintf( ":%s", CONFIG.SERVER_PORT),
-			CONFIG.CERT_FILE,
-			CONFIG.PRIVATE_KEY_FILE, nil))
+		err := startTLS()
+		log.Fatal(err)
 	} else {
-		log.Fatal(http.ListenAndServe( fmt.Sprintf(":%s", CONFIG.SERVER_PORT), nil))
+		err := startNonTLS()
+		log.Fatal(err)
 	}
+}
+
+func startTLS() error {
+	err := http.ListenAndServeTLS(
+		fmt.Sprintf(":%s", CONFIG.SERVER_PORT),
+		CONFIG.CERT_FILE,
+		CONFIG.PRIVATE_KEY_FILE,
+		nil,
+	)
+	return err
+}
+
+func startNonTLS() error {
+	err := http.ListenAndServe(
+		fmt.Sprintf(":%s", CONFIG.SERVER_PORT),
+		nil,
+	)
+	return err
 }
